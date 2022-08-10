7088554901f866460c3ef8bcd9413c57748d71fb
jryambao <johnralph@gmail.com>
2022-08-11 05:44:23 +0800

    random push ulit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
+    // Register function
     public function register(Request $request){
         $validator= Validator::make($request->all(),[
             'name'=>'required|max:191',
