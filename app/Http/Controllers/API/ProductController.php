36edc2a0f64a80bdaa2cfd001fba5bb1581e4ab0
Louie John Manuel Lauro <burshiki@gmail.com>
2022-08-15 18:57:55 +0800

    remove brand change to brand_id

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -41,6 +41,7 @@ class ProductController extends Controller
 
             $product = new Product;
             $product->category_id = $request->input('category_id');
+            $product->brand_id = $request->input('brand_id');
             $product->slug = $request->input('slug');
             $product->name = $request->input('name');
             $product->description = $request->input('description');
@@ -49,7 +50,6 @@ class ProductController extends Controller
             $product->meta_keyword = $request->input('meta_keyword');
             $product->meta_description = $request->input('meta_description');
 
-            $product->brand = $request->input('brand');
             $product->selling_price = $request->input('selling_price');
             $product->original_price = $request->input('original_price');
             $product->qty = $request->input('qty');
@@ -81,7 +81,6 @@ class ProductController extends Controller
             'slug' => 'required|max:191',
             'name' => 'required|max:191',
             'meta_title' => 'required|max:191',
-            'brand' => 'required|max:20',
             'selling_price' => 'required|max:20',
             'original_price' => 'required|max:20',
             'qty' => 'required|max:20',
@@ -100,6 +99,7 @@ class ProductController extends Controller
             if ($product) {
 
                 $product->category_id = $request->input('category_id');
+                $product->brand_id = $request->input('brand_id');
                 $product->slug = $request->input('slug');
                 $product->name = $request->input('name');
                 $product->description = $request->input('description');
@@ -108,7 +108,6 @@ class ProductController extends Controller
                 $product->meta_keyword = $request->input('meta_keyword');
                 $product->meta_description = $request->input('meta_description');
 
-                $product->brand = $request->input('brand');
                 $product->selling_price = $request->input('selling_price');
                 $product->original_price = $request->input('original_price');
                 $product->qty = $request->input('qty');
